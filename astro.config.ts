36fb37f6b35269be45efbd06e28bb1e3e4059d78
lilnasy <69170106+lilnasy@users.noreply.github.com>
2024-10-25 20:41:40 +0530

    carve out vite config

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,31 @@
 import { fileURLToPath } from "node:url"
-import { defineConfig } from "astro/config"
+import { defineConfig, type ViteUserConfig } from "astro/config"
 import node from "@withastro/node"
 import preact from "@preact/preset-vite"
 import { ecsstatic } from "@acab/ecsstatic/vite"
 import precompress from "./precompress.ts"
+
+const vite: ViteUserConfig = {
+    plugins: [ ecsstatic({ classNamePrefix: "e" }), preact() ],
+    ssr: {
+        noExternal: import.meta.env.PROD || undefined
+    },
+    resolve: {
+        alias: {
+            assets: fileURLToPath(new URL("./assets", import.meta.url))
+        }
+    },
+    build: {
+        assetsInlineLimit: 0,
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: '_astro/[hash].mjs',
+                assetFileNames: '_astro/[hash][extname]'
+            }
+        }
+    }
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,25 +37,5 @@ export default defineConfig({
         host: import.meta.env.DEV ? "0.0.0.0" : "127.0.0.1"
     },
     devToolbar: { enabled: false },
-    vite: {
-        plugins: [ ecsstatic({ classNamePrefix: "e" }), preact() ],
-        ssr: {
-            noExternal: import.meta.env.PROD || undefined
-        },
-        resolve: {
-            alias: {
-                assets: fileURLToPath(new URL("./assets", import.meta.url))
-            }
-        },
-        build: {
-            assetsInlineLimit: 0,
-            sourcemap: true,
-            rollupOptions: {
-                output: {
-                    entryFileNames: '_astro/[hash].mjs',
-                    assetFileNames: '_astro/[hash][extname]'
-                }
-            }
-        }
-    }
+    vite
 })
